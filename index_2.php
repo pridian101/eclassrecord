b25eb4a5fd7498d54ee3e7417d839301375fcfc4
Maxx <pridian.101@gmail.com>
2017-10-22 10:08:58 +0000

    removed test files

--- index_2.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include 'misc/header.php' ?>
-
-    <p>Dashboard content goes here</p>
-
-<?php include 'misc/footer.php' ?>
--- namegenerator.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$generator = Nubs\RandomNameGenerator\All::create();
-var_dump($generator->getName());
- ?>
