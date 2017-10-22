c62fa6ae49600ccb000713ffdc491fc253d8b54c
pridian101 <32488218+pridian101@users.noreply.github.com>
2017-10-22 05:08:40 +0000

    Update teacher.php

--- teacher.php
+++ teacher.php
@@ -9,7 +9,7 @@
 		function __construct()
 		{
 			// Create connection
-			$this->conn = new mysqli("db4free.net:3306/mainbookstore", "elective3", "elective3", "elective3");
+			$this->conn = new mysqli("db4free.net:3306/elective3", "elective3", "elective3", "elective3");
 
 			// Check connection
 			if ($this->conn->connect_error) {
