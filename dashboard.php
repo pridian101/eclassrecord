477c587f781d85dd631227f0d0961b15dc771199
pridian101 <32488218+pridian101@users.noreply.github.com>
2017-10-22 06:50:31 +0000

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,12 @@
 <?php
 
-	require('connection.php');
+	$conn = new mysqli("db4free.net:3306/elective3", "elective3", "elective3", "elective3");
+
+	if ($conn->connect_error) {
+
+	    die("Connection failed: " . $conn->connect_error);
+
+	}
 
 	$query = "SELECT first_name, last_name, region, division, district, school_name, school_id FROM teacher WHERE teacher_id=?";
 	$stmt = $conn->prepare($query);
