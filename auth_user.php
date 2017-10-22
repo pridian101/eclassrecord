8db1e9fc468aef957098d82030567686b88e69c4
pridian101 <32488218+pridian101@users.noreply.github.com>
2017-10-22 07:01:34 +0000

    Update auth_user.php

--- auth_user.php
+++ auth_user.php
@@ -1,13 +1,11 @@
 <?php
 
+	session_start();
+
 	$conn = new mysqli("db4free.net:3306/elective3", "elective3", "elective3", "elective3");
 
-
-
 	if ($conn->connect_error) {
-
 	    die("Connection failed: " . $conn->connect_error);
-
 	}
 
 	extract($_POST);
@@ -31,7 +29,7 @@
 		$stmt->close();
 		header("Location: index.php");
 	} else {
-		echo $conn->error);
+		header("Location: login.php");
 	}
 
 ?>
